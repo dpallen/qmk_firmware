0577fbfab8c06e5155d0b6592d64f9926796366f
David Allen <Davidallen417@gmail.com>
2017-01-26 13:36:57 -0500

    Added easier access to braces
    And moved some stuff around on lower.  next: hyper

--- keyboards/preonic/keymaps/cmda/keymap.c
+++ keyboards/preonic/keymaps/cmda/keymap.c
@@ -52,8 +52,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,     KC_DEL},
   {RAISE, KC_A,    KC_S,    KC_D,    KC_F,                KC_G,    KC_H,    KC_J,     KC_K,     KC_L,     KC_SCLN, KC_QUOT},
   {KC_LCTL,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  SFT_T(KC_ENT)},
-  {KC_LSFT,  XXXXXXX, KC_LGUI, KC_LALT, LOWER,   KC_SPC, KC_SPC, RAISE,   KC_MINUS, KC_EQUAL, KC_LBRC,  KC_RBRC}
+  {KC_LSFT,  MOD_HYPR, KC_LGUI, KC_LALT, LOWER,   KC_SPC, KC_SPC, RAISE,   KC_MINUS, KC_EQUAL, KC_LCBR,  KC_RCBR}
 },
+
 /* Colemak
  * ,-----------------------------------------------------------------------------------.
  * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
@@ -70,8 +71,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_GAME] = {
   {KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,     KC_9,     KC_0,     KC_BSPC},
   {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,     _______},
-  {KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT},
-  {KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  _______},
+  {KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT},
+  {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  _______},
   {_______, XXXXXXX, XXXXXXX, _______, _______, KC_SPC,  _______, _______, _______, _______, _______,   _______}
 },
 
@@ -108,11 +109,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Vol- |      |      |      |      |             |      |   +  | Enter|  0   |  .   |
  * `-----------------------------------------------------------------------------------'
  */
+
 [_LOWER] = {
   {KC_GRV,  KC_F1,    KC_F2,   KC_F3,    KC_F4,          KC_F5,   KC_F6,   KC_F7,   KC_F8,    KC_F9,   KC_F10, KC_DEL},
-  {M(2), KC_BSPC,  KC_UP,   KC_DEL,   KC_WWW_FORWARD, _______, _______, _______, _______,  _______,   _______,  _______},
-  {_______, KC_LEFT,  KC_DOWN, KC_RIGHT, KC_WWW_REFRESH, _______, _______, _______, _______,  _______,   _______,  _______},
-  {KC_VOLU, KC_MPRV,  KC_MPLY, KC_MNXT,  KC_WWW_BACK,    _______, _______, _______, _______,  _______,   _______,  _______},
+  {M(2), KC_BSPC,  KC_UP,   KC_DEL,   KC_WWW_FORWARD, _______, _______, _______, _______,  _______,   KC_LBRC,  KC_RBRC},
+  {_______, KC_LEFT,  KC_DOWN, KC_RIGHT, KC_WWW_REFRESH, _______, _______, _______, _______,  _______,   KC_COLN,  KC_PIPE},
+  {KC_VOLU, KC_MPRV,  KC_MPLY, KC_MNXT,  KC_WWW_BACK,    _______, _______, _______, _______,  _______,   KC_QUES,  RSFT(KC_ENT)},
   {KC_VOLD, M(0),  _______, _______,  _______,        _______, _______, _______, _______,  _______, _______,  _______}
 },
 
